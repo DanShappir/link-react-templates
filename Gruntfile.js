1e12a6717e6dd5424d588efcaa5a2ce0de53c525
Dan Shappir <dansh@wix.com>
2014-11-27 13:46:29 +0200

    bower

--- Gruntfile.js
+++ Gruntfile.js
@@ -11,7 +11,7 @@ module.exports = function (grunt) {
                 src: [
                     'src/**/*.js', 
                     '!sample/**/*.js',
-                    '!src/linkrt.browser.js'
+                    '!dist/linkrt.browser.js'
                 ]
             },
             teamcity: {
@@ -22,17 +22,6 @@ module.exports = function (grunt) {
                 src: ['<%= eslint.all.src %>']
             }
         },
-        jasmine_node: {
-            options: {
-                forceExit: true,
-                match: '.',
-                matchall: false,
-                specNameMatcher: 'spec',
-                extensions: 'js'
-            },
-            all: ['server/test'],
-            grunt: ['conf/tasks/test']
-        },
         browserify: {
             options: {
             },
@@ -40,7 +29,7 @@ module.exports = function (grunt) {
             },
             pg: {
                 files: {
-                    'src/linkrt.browser.js': ['src/linkrt.js']
+                    'dist/linkrt.browser.js': ['src/linkrt.js']
                 }
             }
         }
@@ -50,11 +39,9 @@ module.exports = function (grunt) {
     grunt.loadNpmTasks('grunt-eslint');
 
     grunt.registerTask('default', ['eslint', 'browserify']);
-    grunt.registerTask('test', ['jasmine_node']);
 
-    grunt.registerTask('teamcity-check', ['eslint:teamcity'/*, 'scsslint'*/]);
-    grunt.registerTask('teamcity', ['build_sources', 'teamcity-check', 'packages:teamcity', 'static-upload-to-s3']);
-    grunt.registerTask('teamcity-test', ['jasmine_node', 'karma:teamcity', 'cssTest']);
+    grunt.registerTask('teamcity-check', ['eslint:teamcity']);
+    grunt.registerTask('teamcity', ['build_sources', 'teamcity-check']);
 
     grunt.registerTask('all', ['install', 'default']);
 };
